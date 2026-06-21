b4dd397c48ed83e054b09ea5811dea933f92bfa1
unknown <sadiksourov11@gmail.com>
2026-06-22 02:04:00 +0600

    update: router updated

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,34 @@
-import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage.tsx";
 import ProductPage from "../pages/ProductPage.tsx";
 import CartPage from "../pages/CartPage.tsx";
 import CheckoutPage from "../pages/CheckoutPage.tsx";
+import { createBrowserRouter } from "react-router-dom";
+import Mainlayout from "../components/layout/MainLayout.tsx";
+import ErrorPage from "../pages/ErrorPage.tsx";
 
-export default function AppRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/product/:id" element={<ProductPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-    </Routes>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Mainlayout />,
+    errorElement: <ErrorPage />,
+    children: [
+        { 
+            index: true, 
+            Component: HomePage 
+        },
+        { 
+            path: "product/:id", 
+            Component: ProductPage 
+        },
+        { 
+            path: "cart", 
+            Component: CartPage 
+        },
+        { 
+            path: "checkout", 
+            Component: CheckoutPage 
+        },
+    ],
+  },
+]);
+    
